9d7d82ade19a35738e743ba20c6b38890c3bedb4
Jordan LACROIX <jordan.lacroix@free.fr>
2023-04-04 09:47:16 +0200

    [Write-Log] - Add comments

--- Write-Log/Write-Log.ps1
+++ Write-Log/Write-Log.ps1
@@ -1,5 +1,38 @@
 function Write-Log {
     [CmdletBinding()]
+    <#
+.SYNOPSIS
+    Writes a log message to a CSV file and/or the console.
+.DESCRIPTION
+    The Write-Log function writes a log message to a CSV file and/or the console.
+    The log file is saved in the specified log folder with a filename that includes
+    the prefix, the current date formatted according to the specified date format,
+    and the ".csv" file extension. Each log message is saved as a row in the CSV file
+    with the time, source, level, and message as columns.
+.PARAMETER Message
+    The log message to write.
+.PARAMETER Source
+    The source of the log message. By default, the name of the function that called Write-Log.
+.PARAMETER Level
+    The severity level of the log message. Valid values are Info, Warn, Error, Success, and Debug.
+    The default value is Info.
+.PARAMETER HideInConsole
+    If present, the log message will not be displayed on the console.
+.PARAMETER LogFilePrefix
+    The prefix to use in the log file name. The default value is "LOG".
+.PARAMETER LogFolder
+    The folder where log files will be saved. By default, the Logs folder in the same directory as the script.
+.PARAMETER LogFileDateFormat
+    The date format to use in the log file name. The default value is "ddMMyyyy".
+.NOTES
+    Version:        1.0
+    Author:         Jordan LACROIX
+    Creation Date:  2023-04-04
+.EXAMPLE
+    Write-Log -Message "Starting the script"
+    Write-Log -Message "An error occurred" -Level Error
+#>
+
     param(
         [Parameter(Mandatory, ValueFromPipeline)]
         [string]$Message,       
